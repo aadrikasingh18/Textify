8681a4ee222e9392de54d27ff9c8960cf9e83875
aadrikasingh18 <aadrikas16@gmail.com>
2023-12-07 01:50:54 +0530

    Modifications

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,48 +1,52 @@
 import React from 'react'
-import { useState } from 'react'
 
 export default function About() {
 
     //DARK MODE
-    const [myStyle, setmyStyle] = useState({
-        color: 'black',
-        backgroundColor: 'white'
-    })
+    // const [myStyle, setmyStyle] = useState({
+    //     color: 'black',
+    //     backgroundColor: 'white'
+    // })
 
-    const [btnText, setBtnText] = useState("Enable Dark Mode")
+    // const [btnText, setBtnText] = useState("Enable Dark Mode")
 
-    const toggleStyle = () => {
-        if (myStyle.color === 'black') {
-            setmyStyle({
-                color: 'white',
-                backgroundColor: 'black',
-                border: '1px solid white'
-            })
-            setBtnText("Enable Light Mode")
-        }
-        else {
-            setmyStyle({
-                color: 'black',
-                backgroundColor: 'white'
-            })
-            setBtnText("Enable Dark Mode")
-        }
-    }
+    // const toggleStyle = () => {
+    //     if (myStyle.color === 'black') {
+    //         setmyStyle({
+    //             color: 'white',
+    //             backgroundColor: 'black',
+    //             border: '1px solid white'
+    //         })
+    //         setBtnText("Enable Light Mode")
+    //     }
+    //     else {
+    //         setmyStyle({
+    //             color: 'black',
+    //             backgroundColor: 'white'
+    //         })
+    //         setBtnText("Enable Dark Mode")
+    //     }
+    // }
 
     return (
         <div>
-            <div className='container my-3' style={myStyle}>
+        {/* Dark mode ke time use aa rha tha commented part ka */}
+            {/* <div className='container my-3' style={myStyle}> */}
+            <div className='container my-3'>
                 <h3>ABOUT US</h3>
                 <p>TEXTIFY is a text modifying tool. It helps in modifying the text the way you want. Functionalities offered by textify are listed below : </p>
-                <div className="accordion" id="accordionExample" style={myStyle}>
+                {/* <div className="accordion" id="accordionExample" style={myStyle}> */}
+                <div className="accordion" id="accordionExample">
                     <div className="accordion-item">
                         <h2 className="accordion-header" id="headingOne">
-                            <button className="accordion-button" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                            {/* <button className="accordion-button" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne"> */}
+                            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
                                 UPPER CASE
                             </button>
                         </h2>
                         <div id="collapseOne" className="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                            <div className="accordion-body" style={myStyle}>
+                            {/* <div className="accordion-body" style={myStyle}> */}
+                            <div className="accordion-body">
                                 <strong>Converts the whole string to upper case.</strong>
                             </div>
                         </div>
@@ -50,24 +54,24 @@ export default function About() {
 
                     <div className="accordion-item">
                         <h2 className="accordion-header" id="headingTwo">
-                            <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                                 LOWER CASE
                             </button>
                         </h2>
                         <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                            <div className="accordion-body" style={myStyle}>
+                            <div className="accordion-body">
                                 <strong>Converts the whole string to lower case.</strong>
                             </div>
                         </div>
                     </div>
                     <div className="accordion-item">
                         <h2 className="accordion-header" id="headingThree">
-                            <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                                 CAPITALIZED CASE
                             </button>
                         </h2>
                         <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                            <div className="accordion-body" style={myStyle}>
+                            <div className="accordion-body">
                                 <strong>Converts the first letter of every word to upper case.</strong>
                             </div>
                         </div>
@@ -75,12 +79,12 @@ export default function About() {
 
                     <div className="accordion-item">
                         <h2 className="accordion-header" id="headingFour">
-                            <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
                                 INVERSE CASE
                             </button>
                         </h2>
                         <div id="collapseFour" className="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
-                            <div className="accordion-body" style={myStyle}>
+                            <div className="accordion-body">
                                 <strong>Converts the upper case to lower case and vice versa.</strong>
                             </div>
                         </div>
@@ -88,12 +92,12 @@ export default function About() {
 
                     <div className="accordion-item">
                         <h2 className="accordion-header" id="headingFive">
-                            <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
                                 READ
                             </button>
                         </h2>
                         <div id="collapseFive" className="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
-                            <div className="accordion-body" style={myStyle}>
+                            <div className="accordion-body">
                                 <strong>Text is being read by the computer.</strong>
                             </div>
                         </div>
@@ -101,12 +105,12 @@ export default function About() {
 
                     <div className="accordion-item">
                         <h2 className="accordion-header" id="headingSix">
-                            <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
                                 COPY TEXT
                             </button>
                         </h2>
                         <div id="collapseSix" className="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionExample">
-                            <div className="accordion-body" style={myStyle}>
+                            <div className="accordion-body">
                                 <strong>Text is being copied to the clipboard.</strong>
                             </div>
                         </div>
@@ -114,12 +118,12 @@ export default function About() {
 
                     <div className="accordion-item">
                         <h2 className="accordion-header" id="headingSeven">
-                            <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
                                 CLEAR TEXT
                             </button>
                         </h2>
                         <div id="collapseSeven" className="accordion-collapse collapse" aria-labelledby="headingSeven" data-bs-parent="#accordionExample">
-                            <div className="accordion-body" style={myStyle}>
+                            <div className="accordion-body">
                                 <strong>Text is being deleted.</strong>
                             </div>
                         </div>
